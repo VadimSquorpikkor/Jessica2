72cddb6cf813999ba9a711c621287224d0bab547
VadimSquorpikkor <uniqueehub1024>
2021-11-29 16:54:53 +0300

    1.0.0.3 - 29/11/21 1. F2L

--- app/src/main/java/com/squorpikkor/app/jessica2/Data.java
+++ app/src/main/java/com/squorpikkor/app/jessica2/Data.java
@@ -13,49 +13,114 @@ public class Data {
         ArrayList<Algorithm> list = new ArrayList<>();
         list.add(new Algorithm(FTL, R.drawable.f2l01, 1, "1", "U R U' R' U' y L' U L"));
         list.add(new Algorithm(FTL, R.drawable.f2l02, 1, "2", "y U' L' U L U y' R U' R'"));
-        list.add(new Algorithm(FTL, R.drawable.f2l03, 1, "1", "U R U' R'"));
-        list.add(new Algorithm(FTL, R.drawable.f2l04, 1, "1", "y U' L' U L"));
+        list.add(new Algorithm(FTL, R.drawable.f2l03, 1, "1", "U R U' R'\n" +
+                "R' F R F'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l04, 1, "1", "y U' L' U L\n" +
+                "F R' F' R"));
         list.add(new Algorithm(FTL, R.drawable.f2l05, 1, "1", "R U R'"));
         list.add(new Algorithm(FTL, R.drawable.f2l06, 1, "1", "y L' U' L"));
-        list.add(new Algorithm(FTL, R.drawable.f2l07, 1, "1", "U' R U2 R' U2 R U' R'\n" +
-                "U' R U2 R' U' R U2 R'\n" +
-                "U' R U2 R' U R' F R F'"));
-        list.add(new Algorithm(FTL, R.drawable.f2l08, 1, "1", "d R' U' R U2 R' U R\n" +
+        list.add(new Algorithm(FTL, R.drawable.f2l07, 1, "1", "d R' U' R U2 R' U R\n" +
                 "y U L' U' L U2 L' U L\n" +
                 "U' r U' R' U R U r'"));
-        list.add(new Algorithm(FTL, R.drawable.f2l09, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l10, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l11, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l12, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l13, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l14, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l15, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l16, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l17, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l18, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l19, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l20, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l21, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l22, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l23, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l24, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l25, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l26, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l27, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l28, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l29, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l30, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l31, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l32, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l33, 1, "", ""));
+        list.add(new Algorithm(FTL, R.drawable.f2l08, 1, "1", "d R' U2 R U2 R' U R\n" +
+                "r' U2 R2 U R2’ U r\n" +
+                "y U L' U2 L U2 L' U L"));
+        list.add(new Algorithm(FTL, R.drawable.f2l09, 1, "", "U' R U2 R' U2 R U' R'\n" +
+                "U' R U2 R' U' R U2 R'\n" +
+                "U' R U2 R' U R' F R F'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l10, 1, "", "U' R U R' U2 R U' R'\n" +
+                "U' R U R' U' R U2 R'\n" +
+                "U' R U R' U R' F R F'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l11, 1, "", "U' R U R' U R U R'\n" +
+                "U y' R' U R U' y R U R'\n" +
+                "U2 R U' R' U' R U R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l12, 1, "", "U' R U' R' U F' U' F\n" +
+                "U' R U' R' d R' U' R\n" +
+                "F R U R' U' F' R U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l13, 1, "", "U' R U' R' U R U R'\n" +
+                "y L' U2 L U2 F U2 F' U F U' F'\n" +
+                "R U' R' U R U2 R' U2 R U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l14, 1, "", "y' U R' U R U' R' U' R\n" +
+                "d R' U R U' R' U' R\n" +
+                "M' U' R U R' U' R U2 r'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l15, 1, "", "R U' R' U R U' R' U2 R U' R'\n" +
+                "R' U2 R2 U R2 U R\n" +
+                "U F' U2 F U' R U R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l16, 1, "", "U' R U2 R' U F' U' F\n" +
+                "U' R U2 R' d R' U' R\n" +
+                "y' R U2 R2 U' R2 U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l17, 1, "", "R U' R' U2 F' U' F\n" +
+                "R U' R' U2 y L' U' L\n" +
+                "R U' R' U d R' U' R"));
+        list.add(new Algorithm(FTL, R.drawable.f2l18, 1, "", "R U R' U2 R U' R' U R U' R'\n" +
+                "y L' U L U2 y' R U R'\n" +
+                "R U2 R' U R U R' U R U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l19, 1, "", "U2 R' F R F' U2 R U R'\n" +
+                "U F' U' F U' R U R'\n" +
+                "R2 u R U R' U' u' R' U R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l20, 1, "", "U' R U' R' U2 R U' R'\n" +
+                "U' R U' R' U R' F R F'\n" +
+                "R U R' U' R U' R' U R U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l21, 1, "", "U R U R' U2 R U R'\n" +
+                "U' R U2 R' U R U R'\n" +
+                "y U L' U L U2 L' U L"));
+        list.add(new Algorithm(FTL, R.drawable.f2l22, 1, "", "U' R U' R' U2 R U' R'\n" +
+                "U' R U' R' U R' F R F'\n" +
+                "R U R' U' R U' R' U R U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l23, 1, "", "U' R' F R F' R U' R'\n" +
+                "R U' R' y L' U2 L\n" +
+                "l F' R U' R' U l'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l24, 1, "", "R U R' U' R U R' U' R U R'\n" +
+                "U R U' R' U R U' R' U R U' R'\n" +
+                "R2 U R2 U R2 U2 R2"));
+        list.add(new Algorithm(FTL, R.drawable.f2l25, 1, "", "y' U' R' U2 R U' R' U R\n" +
+                "y U' L' U2 L U' L' U L\n" +
+                "U' R U' R2 F R F' R U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l26, 1, "", "F' L' U2 L F\n" +
+                "y' U2 R' U' R U' R' U R\n" +
+                "r U' r' U2 r U r'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l27, 1, "", "U2 R U R' U R U' R'\n" +
+                "R U' R' U2 R U R'\n" +
+                "y F R U2 R' F'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l28, 1, "", "U R U2 R' U R U' R'\n" +
+                "U R U2 R2 F R F'\n" +
+                "R U' R' U R U' R' U R U R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l29, 1, "", "R U2 R' U' R U R'\n" +
+                "y L F' L' F L' U L U' L' U L\n" +
+                "U' R U' R' U2 R U2 R2 F R F'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l30, 1, "", "y' R' U2 R U R' U' R\n" +
+                "y L' U2 L U L' U' L\n" +
+                "R' F R F' R U' R' U R U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l31, 1, "", "R U R' U2 R U R' U' R U R'\n" +
+                "U R U' R' U' R U' R' U R U' R'\n" +
+                "U2 R2 U2 R' U' R U' R2"));
+        list.add(new Algorithm(FTL, R.drawable.f2l32, 1, "", "F U R U' R' F' R U' R'\n" +
+                "U' R U R2 F R F' R U' R'\n" +
+                "y U' L' U L U L' U L U' L' U L"));
+        list.add(new Algorithm(FTL, R.drawable.f2l33, 1, "", "R U R' U' F R' F' R\n" +
+                "y L' U L U' L' U L\n" +
+                "y' R' U R U' R' U R"));
         list.add(new Algorithm(FTL, R.drawable.f2l34, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l35, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l36, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l37, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l38, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l39, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l40, 1, "", ""));
-        list.add(new Algorithm(FTL, R.drawable.f2l41, 1, "", ""));
+        list.add(new Algorithm(FTL, R.drawable.f2l35, 1, "", "R U' R' U R U' R'\n" +
+                "R U' R2 F R F'\n" +
+                "y L' U' L U2 F U' F'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l36, 1, "", "R U R' U' F R' F' R\n" +
+                "y L' U L U' L' U L\n" +
+                "y' R' U R U' R' U R"));
+        list.add(new Algorithm(FTL, R.drawable.f2l37, 1, "", "R U R' U' R U2 R' U' R U R'\n" +
+                "R U' R' U' R U R' U2 R U' R'\n" +
+                "R2 U2 R' U' R U' R' U2 R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l38, 1, "", "R U R' U2 R U' R' U R U R'\n" +
+                "R U' R' U R U2 R' U R U' R'\n" +
+                "R U2 R U R' U R U2 R2"));
+        list.add(new Algorithm(FTL, R.drawable.f2l39, 1, "", "R U' R' F R U R' U' F' R U' R'\n" +
+                "r U' r' U2 r U r' R U R'\n" +
+                "R F U R U' R' F' U' R'"));
+        list.add(new Algorithm(FTL, R.drawable.f2l40, 1, "", "R U R' U' R U' R' U2 y' R' U' R\n" +
+                "R U F R U R' U' F' R'\n" +
+                "R U' R' F' L' U2 L F"));
+        list.add(new Algorithm(FTL, R.drawable.f2l41, 1, "", "R' F R F' R U' R' U R U' R' U2 R U' R'\n" +
+                "R U' R' U y' R' U2 R U2' R' U R\n"+"мой: R U R' Г2 R U2 R' U'(мизинцем) y L' U' L"));
 
         list.add(new Algorithm(OLL, R.drawable.oll01, 1, "", ""));
         list.add(new Algorithm(OLL, R.drawable.oll02, 1, "", ""));
--- app/src/main/java/com/squorpikkor/app/jessica2/MainActivity.java
+++ app/src/main/java/com/squorpikkor/app/jessica2/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
-import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 
